0e8f8357f41e3c68ced37443f84629dff1b4e900
Joel Vargas <joelvargasapolinario@gmail.com>
2022-02-24 12:02:48 -0400

    added user schema

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -3,3 +3,4 @@ export interface IUser extends Document {
     email: string;
     password: string;
   };
+  
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,1 +1,19 @@
-import { model, Schema, Document } from "mongoose";
+import { model, Schema } from "mongoose";
+import { IUser } from "../interfaces/user";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+export default model<IUser>("User", userSchema);
