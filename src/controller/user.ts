7f81935751d750687743a9c5f5eab0215793a3b0
Joel Vargas <joelvargasapolinario@gmail.com>
2022-02-24 12:24:33 -0400

    signToken function

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,3 +1,10 @@
 import { Request, Response } from 'express';
+import { IUser } from "../interfaces/user";
+import  User  from "../models/user";
+import  jwt from "jsonwebtoken";
 
+const createToken =(user:IUser) => 
+{
+    return jwt.sign({ id: user.id, email: user.email }, process.env.jwtSecret || "TopSecret", {expiresIn: 86400});
+}
 
