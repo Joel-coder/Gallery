a76fbb29960a9d2cf22651499cd13a9752c12ef2
Joel Vargas <joelvargasapolinario@gmail.com>
2022-03-07 22:57:44 -0400

    adding authentication and image route

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userRouter from './user';
-
+import imageRouter from './image';
 
 // Export the base-router
 const baseRouter = Router();
 
 baseRouter.use('/', userRouter);
+baseRouter.use('/', imageRouter);
 
 export default baseRouter;
