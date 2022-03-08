665cf78bf55981ccbfa353edfbb966d2566d5261
Joel Vargas <joelvargasapolinario@gmail.com>
2022-03-07 22:03:44 -0400

    adding image view in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleFile = (e) => {
+    console.log(e.target.files[0]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        name="file"
+        onChange={(e) => {
+          handleFile(e);
+        }}
+      ></input>
+      <button type="button">upload</button>
     </div>
   );
 }
